6629b9b42c4d8d5aade555547a3d567807eb5cc1
Mounir <elb_mounir@hotmail.com>
2023-01-25 13:17:46 +0200

    Added countdown

--- p-alpha.js
+++ p-alpha.js
@@ -1,9 +1,19 @@
-//timer count
-const timerCounter = (time) => {
-    while (time >= 0) {
-        setTimeout(timerCounter,1000);
-        console.log(Math.floor(time));
-        time--;
+const count= 1500; // input countdown time in minutes
+// function to count down until  0.
+async function counterDown(count) {
+   count--;
+   while(count >= 0) {
+      let minutes = Math.floor(count/60);
+      let seconds = Math.floor(count- (minutes*60));
+      if (minutes < 10) {
+           minutes = "0" + minutes;
+        }
+      if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+    console.log(`${minutes} : ${seconds}`);
+    await new Promise(resolve => setTimeout(resolve,1000));
+    count--;
     }
-}
-timerCounter(10);
+};
+counterDown(count);
