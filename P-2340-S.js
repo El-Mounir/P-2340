0f2ff7108c814934dfa965f3222e57170b51ac39
Mounir <elb_mounir@hotmail.com>
2023-02-05 14:50:35 +0200

    updated the css/js/htm lines

--- P-2340-S.js
+++ P-2340-S.js
@@ -1,56 +1,48 @@
-//hides all features descriptions and displays the first image.
+//list of available features
+let f1 = document.getElementById("s2ctn2cf1");
+let f2 = document.getElementById("s2ctn2cf2");
+let f3 = document.getElementById("s2ctn2cf3");
+//set first and second features on display as default
 const statis=()=>{
-    if ((document.getElementById("slide1")!="rgb(30, 30, 167)") && ((document.getElementById("slide2")!="rgb(30, 30, 167)")) && (document.getElementById("slide3")!="rgb(30, 30, 167)")) { 
-        document.getElementById("description1").style.display="none";
-        document.getElementById("description2").style.display="none";
-        document.getElementById("description3").style.display="none";
-        document.getElementById("feature2").hidden = true;
-        document.getElementById("feature3").hidden = true;
+    for (let f=0;f < 3 ;f++ ){
+        if(f > 1 ) {
+            f3.style.display="none";
+            document.getElementById("slide2").style.backgroundColor="rgb(255, 255, 255)";
+        } else {
+            f2.style.display="flex";
+            f1.style.display="flex";
+            document.getElementById("slide1").style.backgroundColor="rgb(30, 30, 167)";
+        }
     }
-}
-//will switch the colors of the selected buttons and hide the image and its description
-const switching =(event) =>{
-    if (event.target == document.getElementById("slide1") || (event.target == document.getElementById("feature1"))) {
-        document.getElementById("slide1").style.backgroundColor="rgb(30, 30, 167)";
-        document.getElementById("feature1").hidden= false;
-        document.getElementById("feature2").hidden = true;
-        document.getElementById("feature3").hidden = true;
-        document.getElementById("slide2").style.backgroundColor="rgb(240, 240, 227)";
-        document.getElementById("slide3").style.backgroundColor="rgb(240, 240, 227)";
-        document.getElementById("description1").style.display="inline-flex";
-        document.getElementById("description2").style.display="none";
-        document.getElementById("description3").style.display="none";
-        location.href="#slide1";
-    } else if (event.target == document.getElementById("slide2")||(event.target == document.getElementById("feature2"))) {
-        document.getElementById("slide2").style.backgroundColor="rgb(30, 30, 167)";
-        document.getElementById("feature2").hidden= false;
-        document.getElementById("feature1").hidden = true;
-        document.getElementById("feature3").hidden = true;
-        document.getElementById("slide1").style.backgroundColor="rgb(240, 240, 227)";
-        document.getElementById("slide3").style.backgroundColor="rgb(240, 240, 227)";
-        document.getElementById("description2").style.display="inline-flex";
-        document.getElementById("description1").style.display="none";
-        document.getElementById("description3").style.display="none";
-        location.href="#slide1";
-    } else if (event.target == document.getElementById("slide3")||(event.target == document.getElementById("feature3"))) {
-        document.getElementById("slide3").style.backgroundColor="rgb(30, 30, 167)";
-        document.getElementById("feature3").hidden= false;
-        document.getElementById("feature1").hidden = true;
-        document.getElementById("feature2").hidden = true;
-        document.getElementById("slide1").style.backgroundColor="rgb(240, 240, 227)";
-        document.getElementById("slide2").style.backgroundColor="rgb(240, 240, 227)";
-        document.getElementById("description3").style.display="inline-flex";
-        document.getElementById("description1").style.display="none";
-        document.getElementById("description2").style.display="none";
-        location.href="#slide1";
-    }
-}
-if(window.innerWidth > 568){
-   statis();
-   document.getElementById("slide1").onclick=switching;
-   document.getElementById("slide2").onclick=switching;
-   document.getElementById("slide3").onclick=switching;
-   document.getElementById("feature1").onclick=switching;
-   document.getElementById("feature2").onclick=switching;
-   document.getElementById("feature3").onclick=switching;
 };
+//will slide the features boxes and displays 2 features each time
+const switching =(event) =>{
+    if(event.target == document.getElementById("sec2rtar")|| (event.target == document.getElementById("slide2"))) {
+      for (let f=0;f < 3;f++ ) {
+          if (f3.style.display="none" && f < 2) {
+              f1.style.display="none";
+              f3.style.display="flex";
+              document.getElementById("slide2").style.backgroundColor="rgb(30, 30, 167)";
+              document.getElementById("slide1").style.backgroundColor="rgb(255, 255, 255)";
+            }
+        }
+    };
+    if(event.target == document.getElementById("sec2lfar")||(event.target == document.getElementById("slide1"))) {
+        for (let f=0;f < 3 ;f++ ) {
+          if (f1.style.display="none" && f > 0) {
+              f3.style.display="none";
+              f1.style.display="flex";
+              document.getElementById("slide1").style.backgroundColor="rgb(30, 30, 167)";
+              document.getElementById("slide2").style.backgroundColor="rgb(255, 255, 255)";
+          }
+        }
+    }
+};
+
+
+statis();
+document.getElementById("sec2rtar").onclick=switching;
+document.getElementById("sec2lfar").onclick=switching;
+document.getElementById("slide1").onclick=switching;
+document.getElementById("slide2").onclick=switching;
+
